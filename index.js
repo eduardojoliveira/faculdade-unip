a3e58955f1c551b1af9f1c582d684936bcbe4e75
eduardojoliveira <87249718+eduardojoliveira@users.noreply.github.com>
2021-07-10 21:13:09 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,19 @@
-var dt = new Date();
-var diasem = dt.getDay();
-var dia = dt.getDate();
-var mes = dt.getMonth();
-var ano = dt.getFullYear();
-
-var meses = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", " Dezembro");
-
-var semanas = new Array("Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sabado");
-
-document.write(semanas[diasem] + " , " + dia + " de " + meses[mes] + " de " + ano);
-
-function relogio() {
-
-    var data = new Date();
-    var hor = data.getHours();
-    var min = data.getMinutes();
-    var seg = data.getSeconds();
-
-    if (hor < 10) {
-        hor = "0" + hor;
-    }
-
-    if (min < 10) {
-        min = "0" + min;
-    }
-
-    if (seg < 10) {
-        seg = "0" + seg;
-    }
-
-    var horas = hor + ":" + min + ":" + seg;
-    document.getElementById("rel").value = horas;
-
+ function insert(num){
+	document.form.textview.value = document.form.textview.value
+	+=num;
 }
 
-var tempo = setInterval(relogio, 1000);
+function equal(){
+	var exp = document.form.textview.value;
+	if (exp)
+	document.form.textview.value = eval(exp);
+}
+
+function clean(){
+	document.form.textview.value = "";
+}
+
+function back(){
+	var exp = document.form.textview.value;
+	document.form.textview.value = exp.substring (0, exp.length-1);
+}
